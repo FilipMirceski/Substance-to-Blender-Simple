8df871e6de392d88c5d1775cf2c1e02c3d534fd9
Jelena Jokic <jelena.jokic1701@gmail.com>
2020-03-31 17:28:31 +0200

    Izmena UI

--- create_material_ui.py
+++ create_material_ui.py
@@ -1,12 +1,4 @@
 import bpy
-from bpy_extras.io_utils import ImportHelper
-from bpy.props import StringProperty, BoolProperty, EnumProperty
-from bpy.types import Operator
-
-from . import import_file
-
-import json
-
 
 class HelloWorldPanel(bpy.types.Panel):
     """Creates a Panel in the Object properties window"""
@@ -19,16 +11,60 @@ class HelloWorldPanel(bpy.types.Panel):
     def draw(self, context):
         layout = self.layout
 
-
-
-        row = layout.row()
-        row.operator("import_test.importfile", text="Import file test")
+        obj = context.object
 
         row = layout.row()
-        row.operator("object.createbsdf")
+        row.label(text="Import Material!", icon='WORLD_DATA')
 
+        row = layout.row()
+        row.label(text="Active object is: " + obj.name)
+        row = layout.row()
+        row.prop(obj, "name")
+
+        row = layout.row()
+        row = layout.row()
+        row = layout.row()
+        # Button for Import Base Map
+        row = layout.row()
+        row.operator("mesh.primitive_cube_add", text="Import Base Map")
+
+        
+        # Button for Import Specular Map
+
+        row = layout.row()
+        row.operator("mesh.primitive_cube_add", text="Import Specular Map")
+
+        # Button for Import Metallic Map
+
+        row = layout.row()
+        row.operator("mesh.primitive_cube_add", text="Import Metallic Map")
+
+        # Button for Import Roughness Map
+
+        row = layout.row()
+        row.operator("mesh.primitive_cube_add", text="Import Roughness Map")
+
+        # Button for Import DIsplacement Map
+        
+        row = layout.row()
+        row.operator("mesh.primitive_cube_add", text="Import DIsplacement Map")
+
+        # Button for Import Normal Map
+
+        row = layout.row()
+        row.operator("mesh.primitive_cube_add", text="Import Normal Map")
+
+        
+        row = layout.row()
+        row = layout.row()
+        row = layout.row()
+        # Button for Create BSDF Material
+        row = layout.row()
+        row.scale_y = 3.0
+        row.operator("mesh.primitive_cube_add", text="Create BSDF Material")
 
       
+
 
 def register():
     bpy.utils.register_class(HelloWorldPanel)
