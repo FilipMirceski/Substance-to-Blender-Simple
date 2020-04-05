062f9464b686d592927598c6031b7c717ec8e85c
KristinaPetrov <kristinapetrov23@gmail.com>
2020-04-06 00:44:55 +0200

    Import file uradjen

--- create_material_ui.py
+++ create_material_ui.py
@@ -26,33 +26,33 @@ class HelloWorldPanel(bpy.types.Panel):
         row = layout.row()
         # Button for Import Base Map
         row = layout.row()
-        row.operator("mesh.primitive_cube_add", text="Import Base Map")
+        row.operator("import_test.base_color")
 
         
         # Button for Import Specular Map
 
         row = layout.row()
-        row.operator("mesh.primitive_cube_add", text="Import Specular Map")
+        row.operator("import_test.specular")
 
         # Button for Import Metallic Map
 
         row = layout.row()
-        row.operator("mesh.primitive_cube_add", text="Import Metallic Map")
+        row.operator("import_test.metallic")
 
         # Button for Import Roughness Map
 
         row = layout.row()
-        row.operator("mesh.primitive_cube_add", text="Import Roughness Map")
+        row.operator("import_test.roughness")
 
         # Button for Import DIsplacement Map
         
         row = layout.row()
-        row.operator("mesh.primitive_cube_add", text="Import DIsplacement Map")
+        row.operator("import_test.displacement")
 
         # Button for Import Normal Map
 
         row = layout.row()
-        row.operator("mesh.primitive_cube_add", text="Import Normal Map")
+        row.operator("import_test.normal")
 
         
         row = layout.row()
@@ -61,7 +61,7 @@ class HelloWorldPanel(bpy.types.Panel):
         # Button for Create BSDF Material
         row = layout.row()
         row.scale_y = 3.0
-        row.operator("mesh.primitive_cube_add", text="Create BSDF Material")
+        row.operator("object.createbsdf")
 
       
 
--- import_file.py
+++ import_file.py
@@ -3,13 +3,54 @@ import json
 
 from . import create_mat
 
-def test_import_file(context,filepath):
+def read_base_color(context,filepath):
 
     b = filepath
-    
     print(filepath)
+    create_mat.base_color(context, filepath)
 
     return {'FINISHED'}
+
+
+def read_specular(context,filepath):
+
+    b = filepath
+    print(filepath)
+    create_mat.specular(context, filepath)
+
+    return {'FINISHED'}
+
+def read_metallic(context,filepath):
+
+    b = filepath
+    print(filepath)
+    create_mat.metallic(context, filepath)
+
+    return {'FINISHED'}
+
+def read_roughness(context,filepath):
+
+    b = filepath
+    print(filepath)
+    create_mat.roughness(context, filepath)
+
+    return {'FINISHED'}
+def read_displacement(context,filepath):
+
+    b = filepath
+    print(filepath)
+    create_mat.displacement(context, filepath)
+
+    return {'FINISHED'}
+
+def read_normal(context,filepath):
+
+    b = filepath
+    print(filepath)
+    create_mat.normal(context, filepath)
+
+    return {'FINISHED'}
+
 
 # ImportHelper is a helper class, defines filename and
 # invoke() function which calls the file selector.
@@ -18,17 +59,79 @@ from bpy.props import StringProperty, BoolProperty, EnumProperty
 from bpy.types import Operator
 
 
-class ImportTestFile(Operator, ImportHelper):
+class ImportBaseColor(Operator, ImportHelper):
     """This appears in the tooltip of the operator and in the generated docs"""
-    bl_idname = "import_test.importfile"  # important since its how bpy.ops.import_test.some_data is constructed
+    bl_idname = "import_test.base_color"  # important since its how bpy.ops.import_test.some_data is constructed
     bl_label = "Import Base Color"
 
 
     def execute(self, context):
-        return test_import_file(context,self.filepath)
+        return read_base_color(context,self.filepath)
+
+class ImportSpecular(Operator, ImportHelper):
+    """This appears in the tooltip of the operator and in the generated docs"""
+    bl_idname = "import_test.specular"  # important since its how bpy.ops.import_test.some_data is constructed
+    bl_label = "Import Speculat Map"
+
+
+    def execute(self, context):
+        return read_specular(context,self.filepath)
+
+class ImportMetallic(Operator, ImportHelper):
+    """This appears in the tooltip of the operator and in the generated docs"""
+    bl_idname = "import_test.metallic"  # important since its how bpy.ops.import_test.some_data is constructed
+    bl_label = "Import Metallic Map"
+
+
+    def execute(self, context):
+        return read_metallic(context,self.filepath)
+
+class ImportRoughness(Operator, ImportHelper):
+    """This appears in the tooltip of the operator and in the generated docs"""
+    bl_idname = "import_test.roughness"  # important since its how bpy.ops.import_test.some_data is constructed
+    bl_label = "Import Roughness Map"
+
+
+    def execute(self, context):
+        return read_roughness(context,self.filepath)
+
+class ImportDisplacement(Operator, ImportHelper):
+    """This appears in the tooltip of the operator and in the generated docs"""
+    bl_idname = "import_test.displacement"  # important since its how bpy.ops.import_test.some_data is constructed
+    bl_label = "Import Displacement Map"
+
+
+    def execute(self, context):
+        return read_displacement(context,self.filepath)
+
+class ImportNormal(Operator, ImportHelper):
+    """This appears in the tooltip of the operator and in the generated docs"""
+    bl_idname = "import_test.normal"  # important since its how bpy.ops.import_test.some_data is constructed
+    bl_label = "Import Normal Map"
+
+
+    def execute(self, context):
+        return read_normal(context,self.filepath)
+
+
+
+
 
 def register():
-    bpy.utils.register_class(ImportTestFile)
+    bpy.utils.register_class(ImportBaseColor)
+    bpy.utils.register_class(ImportSpecular)
+    bpy.utils.register_class(ImportMetallic)
+    bpy.utils.register_class(ImportRoughness)
+    bpy.utils.register_class(ImportDisplacement)
+    bpy.utils.register_class(ImportNormal)
+
+
+
 
 def unregister():
-    bpy.utils.unregister_class(ImportTestFile)
+    bpy.utils.unregister_class(ImportBaseColor)
+    bpy.utils.unregister_class(ImportSpecular)
+    bpy.utils.unregister_class(ImportMetallic)
+    bpy.utils.unregister_class(ImportRoughness)
+    bpy.utils.unregister_class(ImportDisplacement)
+    bpy.utils.unregister_class(ImportNormal)
